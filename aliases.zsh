c7b0069f48737520d6314a30415112652183dbb1
Jeff Pace <jeugenepace@gmail.com>
2012-10-01 08:35:03 -0400

    Added git aliases.

--- aliases.zsh
+++ aliases.zsh
@@ -91,6 +91,8 @@ alias gitst='git status'
 
 alias gitpullall='git pull origin master; git submodule foreach git pull origin master'
 alias gitpushall='git push -u origin master; git submodule foreach git push -u origin master'
+alias gpa='gitpushall'
+alias gpull='gitpullall'
 
 alias gpom='git push -u origin master'
 
