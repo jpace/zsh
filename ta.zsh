499188845e226211f5df1f84601245a4f65554da
Jeff Pace <jeugenepace@gmail.com>
2015-11-03 12:19:37 -0500

    Add ta.zsh as separate file from aliases.zsh

---
+++ ta.zsh
@@ -0,0 +1,26 @@
+# see http://www.focusonzsh.org/2015/08/caching-command-output.html
+
+alias -g TA='| tee /tmp/cmdoutput | cat -n' 
+
+ta() { 
+    arg=$1
+    if [ -z "$arg" ]
+    then
+	tail -1 /tmp/cmdoutput
+    else
+	a=(${(s.:.)arg})
+	first=$a[1]
+	second=$a[2]
+	if [ -z $second ]
+	then
+	    tail -n +$first /tmp/cmdoutput | head -1
+	elif [[ $second[1] = "-" ]]
+	then
+	    cmd="{print \$(NF + 1 + $second)}"
+	    tail -n +$first /tmp/cmdoutput | head -1 | awk "$cmd"
+	else
+	    cmd="{print \$$second}"
+	    tail -n +$first /tmp/cmdoutput | head -1 | awk "$cmd"
+	fi
+    fi
+}
