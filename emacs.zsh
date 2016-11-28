24ab99f4558e83ac661800817bf20946ba595042
Jeff Pace <jeugenepace@gmail.com>
2016-11-28 09:13:34 -0500

    Move ec (function) to emacs code

--- emacs.zsh
+++ emacs.zsh
@@ -1,3 +1,6 @@
+#!/usr/bin/zsh
+# -*- sh -*-
+
 # changes to the directory of the current buffer in Emacs
 # credit: http://chneukirchen.org/blog/archive/2015/02/10-fancy-zsh-tricks-you-may-not-know.html
 cde () {
@@ -12,3 +15,11 @@ otest() {
     o $f:h:gs/main/test//Test$f:t
 }
 
+# This is a function instead of an alias, because "emacsclient --no-wait foo.rb bar.txt" only opens
+# the first file.
+ec() {
+    for i in $*
+    do
+        emacsclient --no-wait $i
+    done
+}
