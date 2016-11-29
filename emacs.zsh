19dc3ccde32554213995303ea96f440e79fddd41
Jeff Pace <jeugenepace@gmail.com>
2016-11-29 08:57:20 -0500

    Add ecf and ecd, getting the current Emacs buffer file/directory name

--- emacs.zsh
+++ emacs.zsh
@@ -1,12 +1,20 @@
 #!/usr/bin/zsh
 # -*- sh -*-
 
+# the current file in Emacs:
+ecf() {
+    echo ${(Q)~$(emacsclient -e '(with-current-buffer (window-buffer (selected-window)) buffer-file-name) ')}
+}
+
+# the directory of the current file in Emacs (same as "ecf | xargs dirname"):
+ecd() {
+    echo ${(Q)~$(emacsclient -e '(with-current-buffer (window-buffer (selected-window)) default-directory) ')}
+}
+
 # changes to the directory of the current buffer in Emacs
 # credit: http://chneukirchen.org/blog/archive/2015/02/10-fancy-zsh-tricks-you-may-not-know.html
 cde () {
-    cd ${(Q)~$(emacsclient -e '(with-current-buffer
-                               (window-buffer (selected-window))
-                               default-directory) ')}
+    cd `ecd`
 }
 
 # create a java test for the current java source file
