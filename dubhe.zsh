ae6d3216880f9f0261beab0dfcf0a1d8806cd561
Jeff Pace <jeugenepace@gmail.com>
2017-08-11 09:27:12 -0400

    Refine gtest to run the latest changed code/test.

--- dubhe.zsh
+++ dubhe.zsh
@@ -4,7 +4,13 @@
 export GRADLE_HOME=/opt/gradle-1.12
 export GROOVY_HOME=/usr/share/groovy2
 
-for i in /opt/org/incava/*
+for i in /opt/org/incava/*(/)
+do
+    n=$i:t
+    hash -d $n=$i
+done
+
+for i in /opt/com/sag/*(/)
 do
     n=$i:t
     hash -d $n=$i
--- gradle.zsh
+++ gradle.zsh
@@ -1,12 +1,21 @@
 # -*- sh -*-
 
-alias gct='gradle clean test'
-alias gr='gradle '
-alias gb='gradle build'
-alias gt='gradle test'
+alias gct='gradle clean test --offline'
+alias gr='gradle --offline'
+alias gb='gradle build --offline'
+alias gt='gradle test --offline'
 
-gst() { gradle test -Dtest.single=$* }
+gst() { gradle test --offline --tests $* }
 
-gtest() { f=`echo src/**/*.java(om[1])`; e=Test$f:gs/Test//:t:r; echo $e; gst $e }
+gtest() {
+    f=`echo src/**/*.java(om[1])`
+    f=$f:r:s/src\/test\/java\//
+    f=$f:r:s/src\/main\/java\//
+    f=$f:gs/\//./
+    f=$f:gs/Test//Test
+    echo $f
+    gst $f
+}
 
-path=(/opt/org/gradle/gradle-4.0/bin $path)
+# path=(/opt/org/gradle/gradle-4.0/bin $path)
+path=(/opt/org/gradle/gradle-3.4.1/bin $path)
