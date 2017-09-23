ce8e7eaa74a3e1f874f67980a688e110b8c3a8d2
Jeff Pace <jeugenepace@gmail.com>
2017-09-23 12:27:51 -0400

    Add "em cmd", a front-end to all Zsh/Emacs scripts.

--- emacs.zsh
+++ emacs.zsh
@@ -2,31 +2,21 @@
 # -*- sh-mode -*-
 
 # the current file in Emacs:
-ecf() {
-    echo ${(Q)~$(emacsclient -e '(with-current-buffer (window-buffer (selected-window)) buffer-file-name) ')}
-}
+alias ecf='em-current-file'
 
 # the directory of the current file in Emacs (same as "ecf | xargs dirname"):
-ecd() {
-    echo ${(Q)~$(emacsclient -e '(with-current-buffer (window-buffer (selected-window)) default-directory) ')}
-}
+alias ecd='em-current-directory'
 
 # the rootname of the current file in Emacs:
-ecr() {
-    x=`ecf`
-    echo $x:t:r
-}
+alias ecr='em-current-rootname'
 
 # the basename of the current file in Emacs:
-ecb() {
-    x=`ecf`
-    echo $x:t
-}
+alias ecb='em-current-basename'
 
 # changes to the directory of the current buffer in Emacs
 # credit: http://chneukirchen.org/blog/archive/2015/02/10-fancy-zsh-tricks-you-may-not-know.html
 cde () {
-    cd `ecd`
+    cd `em-current-directory`
 }
 
 # create a java test for the current java source file
@@ -37,22 +27,19 @@ otest() {
 
 # This is a function instead of an alias, because "emacsclient --no-wait foo.rb bar.txt" only opens
 # the first file.
-ec() {
-    for i in $*
-    do
-        emacsclient --no-wait $i
-    done
-}
+alias ec='em-open'
 
 # currently open file in Emacs:
-alias -g E='`ecf`'
-alias -g EF='`ecf`'
+alias -g E='`em-current-file`'
+alias -g EF='`em-current-file`'
 
 # directory of the current file
-alias -g ED='`ecd`'
+alias -g ED='`em-current-directory`'
 
 # rootname of the current file
 alias -g ER='`ecr`'
 
 # basename of the current file
 alias -g EB='`ecb`'
+
+# compdef _em em
